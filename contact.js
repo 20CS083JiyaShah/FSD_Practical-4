b1e32725a879bdd0cfe355e918d05f15a0831e56
20CS083JiyaShah <91939547+20CS083JiyaShah@users.noreply.github.com>
2022-08-28 22:59:53 +0530

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Home = () => {
+const Contact = () => {
   return (
     <div
       style={{
@@ -10,9 +10,10 @@ const Home = () => {
         height: '90vh'
       }}
     >
-      <h1>Home</h1>
+      <h1>Contact</h1>
     </div>
+    
   );
 };
 
-export default Home;
+export default Contact;
